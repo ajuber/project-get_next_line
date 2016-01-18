4a1297024f0c98b2a2c43d0c5ff94538514f83a2
Anthony JUBERT <ajubert@e2r6p8.42.fr>
2016-01-18 16:16:36 +0100

    reste leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ajubert <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 15:06:11 by ajubert           #+#    #+#             */
-/*   Updated: 2015/12/23 13:04:21 by ajubert          ###   ########.fr       */
+/*   Updated: 2016/01/18 16:07:48 by ajubert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,15 +55,59 @@ void	get_previous_str(char **str)
 	}
 }
 
+void	next_calc(int size, char **line, char **str, int *test)
+{
+	int i;
+
+	if (size == 0)
+		size = ft_strlen(str[0]);
+	line[0] = (char *)malloc(size);
+	line[0] = ft_strcpy(line[0], str[0]);
+	i = 0;
+	while (line[0][i] && line[0][i] != '\n')
+		i++;
+	while (line[0][i])
+	{
+		line[0][i] = '\0';
+		i++;
+		*test = 1;
+	}
+}
+
+int		calc_get_next_line(char **str, int fd, char **line, int *fd1)
+{
+	int test;
+	int strsize;
+	int i;
+	int size;
+
+	while (1)
+	{
+		test = 0;
+		strsize = extractbuffer(str, fd);
+		if (strsize == -1)
+			return (-1);
+		if (str[0][0] == 0)
+		{
+			line[0] = (char *)malloc(1);
+			line[0][0] = '\0';
+			return (0);
+		}
+		size = strsize;
+		next_calc(size, line, str, &test);
+		*fd1 = fd;
+		if ((line[0][i] == '\0' && i < BUFF_SIZE) || test == 1 || strsize == 0)
+			return (1);
+	}
+}
+
 int		get_next_line(int const fd, char **line)
 {
 	static char		**str = 0;
 	static int		fd1 = -1;
-	int				strsize;
-	int				i;
 	int				test;
 
-	if (fd < 0 || line == NULL) 
+	if (fd < 0 || line == NULL || BUFF_SIZE <= 0)
 		return (-1);
 	if (fd1 != fd)
 	{
@@ -81,32 +125,6 @@ int		get_next_line(int const fd, char **line)
 			return (1);
 		}
 	}
-	while (1)
-	{
-		test = 0;
-		strsize = extractbuffer(str, fd);
-		if (strsize == -1)
-			return (-1);
-		if (strsize == 0 && str[0][0] == '\0')
-		{
-			line[0] = (char *)malloc(1);
-			line[0][0] = '\0';
-			return (0);
-		}
-		line[0] = (char *)malloc(strsize);
-		line[0] = ft_strcpy(line[0], str[0]);
-		i = 0;
-		while (line[0][i] && line[0][i] != '\n')
-			i++;
-		while (line[0][i])
-		{
-			line[0][i] = '\0';
-			i++;
-			test = 1;
-		}
-		fd1 = fd;
-		if ((line[0][i] == '\0' && i < BUFF_SIZE - 1) || test == 1)
-			return (1);
-	}
-	return (1);
+	test = calc_get_next_line(str, fd, line, &fd1);
+	return (test == 1 ? 1 : (test == 0 ? 0 : -1));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,21 +6,22 @@
 /*   By: ajubert <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/14 13:35:51 by ajubert           #+#    #+#             */
-/*   Updated: 2015/12/23 13:11:39 by ajubert          ###   ########.fr       */
+/*   Updated: 2016/01/18 16:06:23 by ajubert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include<sys/types.h>
-# include<sys/uio.h>
-# include<unistd.h>
-# include<stdlib.h>
-# include<string.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
 # include <fcntl.h>
-# include"libft.h"
+# include "libft.h"
 
-#define BUFF_SIZE 10
+# define BUFF_SIZE 10
 
 int		get_next_line(int const fd, char **line);
 
